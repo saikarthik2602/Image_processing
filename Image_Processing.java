2c9558882868cbf8176db3a8d2aa551f078525a3
saikarthik2602 <bsaikarthik@am.students.amrita.edu>
2021-01-15 21:54:52 +0530

    Added author name in the java file(line 10)

--- Image_Processing.java
+++ Image_Processing.java
@@ -6,6 +6,7 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage; 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+
 // Bonagiri Sai Karthik
 public class Image_Processing
 {
